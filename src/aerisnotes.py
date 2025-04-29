2e3ffa4d69d4a60355056f02d3eccfbf440ededc
zer0burn86 <pascalwengi@gmail.com>
2025-04-29 02:06:16 +0200

    Implementierung Funktion eingabe_erfassen

--- src/aerisnotes.py
+++ src/aerisnotes.py
@@ -2,11 +2,18 @@
 # Autor: Pascal Wengi
 # Start: Mai 2025
 
-def begruessung():
-    print("Willkommen bei AerisNotes")
+def begruessung(): 
+    print("Willkommen bei AerisNotes") # TBD wird durch UI ersetzt
 
 def eingabe_erfassen():
-    pass
+    text = input("Teile mir deine Idee mit: ") 
+
+    if text.stip() != "": # Kurze Abfrage ob Eingabe einfach aus Leerzeichen besteht
+        print("Danke für deine Idee, ich übergebe sie an OpenAI. Einen Moment bitte...")
+        return text # übergabe an Funktion anfrage_an_openai()
+    else:
+        print("Oups, da ist was schief gelaufen. Bitte teile mir deine Idee nochmal mit.")
+        return eingabe_erfassen()
 
 def anfrage_an_openai(text):
     pass
