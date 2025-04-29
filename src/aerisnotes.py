4c82bc07d1f9ffe042c7d84b97783895b6714a3b
zer0burn86 <pascalwengi@gmail.com>
2025-04-29 19:16:22 +0200

    Einbundung OpenAI mit sicherer Key-Verwaltung und .gitignore

--- src/aerisnotes.py
+++ src/aerisnotes.py
@@ -1,6 +1,13 @@
 # AerisNotes - Wissensmanagementsystem
 # Autor: Pascal Wengi
 # Start: Mai 2025
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+
+openai.api.key = os.getenv("OPENAI_API_KEY")
 
 def begruessung(): 
     print("Willkommen bei AerisNotes") # TBD wird durch UI ersetzt
@@ -8,9 +15,9 @@ def begruessung():
 def eingabe_erfassen():
     text = input("Teile mir deine Idee mit: ") 
 
-    if text.stip() != "": # Kurze Abfrage ob Eingabe einfach aus Leerzeichen besteht
+    if text.stip() != "": # Kurze Abfrage ob Eingabe einfach aus Leerzeichen besteht ("!=" = "ist nicht")
         print("Danke für deine Idee, ich übergebe sie an OpenAI. Einen Moment bitte...")
-        return text # übergabe an Funktion anfrage_an_openai()
+        return text
     else:
         print("Oups, da ist was schief gelaufen. Bitte teile mir deine Idee nochmal mit.")
         return eingabe_erfassen()
